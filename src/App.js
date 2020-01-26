6d4224407129756545ffcd246e1461c4db5984d0
raadu <raiyad.raad@gmail.com>
2020-01-26 18:25:01 +0600

    tutorial files added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+//make new context
+const MyContext = React.createContext();
+
+//make provider component, Data lives here
+class MyProvider extends Component 
+{
+  state={
+    name: "Raad",
+    age: 25,
+    cool: true
+  }
+
+  render()
+  {
+    return(
+      <MyContext.Provider value={{
+        state: this.state,
+        growAYearOlder: () => this.setState({
+          age: this.state.age+1
+        })
+      }}>
+        {this.props.children}
+      </MyContext.Provider>
+    );
+  }
+
+}
+
+const Family = (props) =>
+{
+  return(
+    <div className="family">
+      <Person/>
     </div>
   );
+}
+class Person extends Component 
+{
+  render()
+  {
+    return(
+      <div className="person">
+        <MyContext.Consumer>
+          {(context) => (
+            <div>
+              <p>I'm inside consumer {context.state.name}</p>
+              <p>Age: {context.state.age}</p>
+              <button onClick={context.growAYearOlder}>Increase Age</button>
+            </div>
+          )}
+        </MyContext.Consumer>
+      </div>
+    );
+  }
+}
+class App extends Component 
+{
+  render()
+  {
+    return(
+      <MyProvider>
+        <div>
+          <p>I'm Raad</p>
+          <Family/>
+        </div>
+      </MyProvider>
+     
+    );
+  }
 }
 
 export default App;
